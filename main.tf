bf4bf2c84b678163fd2568410095796cfd1ecc32
rahul-infodesk <117075953+rahul-infodesk@users.noreply.github.com>
2022-11-10 13:40:25 +0530

    Update main.tf
    Terminate this testing instance.

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "server-1" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.nano"
-  tags = {
-    Name = "infodesk-server-2"
-  }
-}
+# resource "aws_instance" "server-1" {
+#   ami           = "ami-830c94e3"
+#   instance_type = "t2.nano"
+#   tags = {
+#     Name = "infodesk-server-2"
+#   }
+# }
