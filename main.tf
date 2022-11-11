ecfb1d1448d90b3ec3be18233d8651c705fab5a2
rahul-infodesk <117075953+rahul-infodesk@users.noreply.github.com>
2022-11-11 21:36:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,6 @@ resource "aws_instance" "server-1" {
   ami           = "ami-830c94e3"
   instance_type = "t2.nano"
   tags = {
-    Name = "infodesk-server-2"
+    Name = "infodesk-server-3"
   }
 }
